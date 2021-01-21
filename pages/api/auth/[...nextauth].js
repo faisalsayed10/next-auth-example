053411fdfc270c913b1708eaf21c0e20b918225c
faisalsayed10 <faisal.sayed502@gmail.com>
2021-01-21 12:06:29 +0530

    wrapping up

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -11,18 +11,24 @@ const options = {
       clientId: process.env.TWITTER_ID,
       clientSecret: process.env.TWITTER_SECRET,
     }),
-    // Providers.Email({
-    //   server: {
-    //     host: "",
-    //     port: "",
-    //     auth: {
-    //       user: "",
-    //       pass: "",
-    //     },
-    //   },
-    //   from: ""
-    // }),
+    Providers.Auth0({
+      clientId: process.env.AUTH0_CLIENT_ID,
+      clientSecret: process.env.AUTH0_CLIENT_SECRET,
+      domain: process.env.AUTH0_DOMAIN,
+    }),
+    Providers.Email({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    }),
   ],
+  database: process.env.DATABASE_URL,
 };
 
 export default (req, res) => NextAuth(req, res, options);
